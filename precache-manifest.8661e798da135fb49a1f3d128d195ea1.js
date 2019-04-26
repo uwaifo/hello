e9dc4673cd12fc8515072a7c1df62bb3ee6d01d7
uwaifo <mr.uwaifo@gmail.com>
2019-04-26 10:35:54 +0100

    Updates

---
+++ precache-manifest.8661e798da135fb49a1f3d128d195ea1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e7cf9220dcbc6a51d002ce89b5e4e7",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "3fac22e6a399e66cff6f",
+    "url": "/hello/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/hello/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "3fac22e6a399e66cff6f",
+    "url": "/hello/static/js/main.e8a6461d.chunk.js"
+  },
+  {
+    "revision": "ded4da0fe71cbdcab24f",
+    "url": "/hello/static/js/runtime~main.f3b3c316.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  }
+]);
